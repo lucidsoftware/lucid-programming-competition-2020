23e5b7b64bd8bc3774faed05a9aa200d8a48a907
Deepak Kancharla <dkancharla@lucidchart.com>
2020-10-08 14:44:25 -0600

    galactic profits python solution updated

--- problems/GalacticProfits/solutions/python3/main.py
+++ problems/GalacticProfits/solutions/python3/main.py
@@ -16,34 +16,20 @@ def profits():
         profit_max = 0
         loss_max = 0
         for month in range(n_months):
-            budget_p = 5000
-            budget_l = 5000
-            while True:
-                profit = 0
-                loss = 0
-                p_p = 0
-                p_l = 0
-
-                for stock in stocks:
-                    ns = budget_p//stocks[stock][month]
-                    net = (stocks[stock][n_months-1] - stocks[stock][month]) * ns
-                    if net>profit:
-                        profit = net
-                        p_p = stocks[stock][month] * ns
-                        
-                    ns = budget_l//stocks[stock][month]
-                    net = (stocks[stock][n_months-1] - stocks[stock][month]) * ns
-
-                    if net<loss:
-                        loss = net
-                        p_l = stocks[stock][month] * ns
-                       
-                if profit==0 and loss==0:
-                    break               
-                budget_p-=p_p
-                budget_l-=p_l
-                profit_max+= profit
-                loss_max+= loss
+            budget = 5000
+            profit = 0
+            loss = 0 
+            for stock in stocks:
+                ns = budget//stocks[stock][month]
+                net = (stocks[stock][n_months-1] - stocks[stock][month]) * ns
+                profit = max(net,profit)
+                    
+                ns = budget//stocks[stock][month]
+                net = (stocks[stock][n_months-1] - stocks[stock][month]) * ns
+                loss = min(net,loss) 
+                              
+            profit_max+= profit
+            loss_max+= loss
         print("Max: ",profit_max)
         print("Min: ",loss_max)
 
