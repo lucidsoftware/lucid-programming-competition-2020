dfbe60b72ef57edd980397586b7aec63249fd842
krrg <krrgithub@gmail.com>
2020-10-10 23:04:12 -0600

    Leaderboard

--- new-leaderboard/handler.ts
+++ new-leaderboard/handler.ts
@@ -1,11 +1,45 @@
-import express from "serverless-express/express";
-import handler from "serverless-express/handler";
 import { getHtml } from "./leaderboard";
 
-const app = express();
+import express from "express";
+import awsServerlessExpressMiddleware from "aws-serverless-express/middleware";
+import awsServerlessExpress from "aws-serverless-express";
 
-app.get("/leaderboard", (req, res) => {
+const app = express();
+const router = express.Router();
+
+router.use(awsServerlessExpressMiddleware.eventContext());
+
+router.get("/leaderboard", (req, res) => {
   getHtml().then(html => res.send(html));
 })
 
-export const api = handler(app);
+router.get("/leaderboard/health_check", (req, res) => {
+  res.send({"alive": true, "request": req.query});
+})
+
+app.use("/programming-competition/", router);
+
+const binaryMimeTypes = [
+  'application/javascript',
+  'application/json',
+  'application/octet-stream',
+  'application/xml',
+  'font/eot',
+  'font/opentype',
+  'font/otf',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+  'text/comma-separated-values',
+  'text/css',
+  'text/html',
+  'text/javascript',
+  'text/plain',
+  'text/text',
+  'text/xml'
+]
+
+const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
+export const api = (event, context) => {
+  return awsServerlessExpress.proxy(server, event, context);
+}
